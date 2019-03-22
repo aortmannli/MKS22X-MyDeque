8d25af8c34d61d81f561b5f22c462c25bdcbd31a
Angel Ortmann-Li <ali02@stuy.edu>
2019-03-22 09:53:19 -0400

    resize and add to front

--- MyDeque.java
+++ MyDeque.java
@@ -4,10 +4,16 @@ public class MyDeque<E>{
 
   public MyDeque(){
     data = (E[])new Object[10];
+    size = 0;
+    start = 0;
+    end = 0;
   }
 
   public MyDeque(int initialCapacity){
     data = (E[])new Object[initialCapacity];
+    size = 0;
+    start = 0;
+    end = 0;
   }
 
   public int size(){
@@ -16,16 +22,34 @@ public class MyDeque<E>{
 
   public String toString(){
     String out = "{";
-    int idx = start; //starts at start
-    for (int i = 0; i < size; i++){ //loops through number of elements
-      out += data[idx % data.length] + " "; //add element modded by data.length
+    int idx = start;
+    for (int i = 0; i < size; i++){
+      out += data[idx % data.length] + " ";
       idx++;
     }
     return out + "}";
   }
 
+  private void resize(){
+    E[] hold = (E[]) new Object[size() * 2 + 1];
+    int idx = start;
+    for (int i = 0; i < size; i++){
+      hold[i] = data[idx % data.length];
+      idx++;
+    }
+    data = hold;
+    start = 0;
+    end = size - 1;
+  }
+  .
   public void addFirst(E element){
-
+    if(size == data.length) resize;
+    if(size != 0){
+      if(start == 0) start = data.length - 0;
+      else start--;
+    }
+    data[start] = element;
+    size++;
   }
 
   public void addLast(E element){
