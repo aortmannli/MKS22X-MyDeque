ad7a239f01f5d6827874c33916b6346b8261744a
Angel Ortmann-Li <ali02@stuy.edu>
2019-03-25 10:16:24 -0400

    started on calculator, end of class

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator{
+    /*Evaluate a postfix expression stored in s.
+     *Assume valid postfix notation, separated by spaces.
+     */
+    static String[] op = {"+", "-", "*", "/"}
+    public static double eval(String s){
+      String[] in = String.split(s);
+      MyDeque<String> ans = new MyDeque<String>(in.length);
+      for(String i : in){
+        if(operations.contains(i)){
+
+        }
+      }
+    }
+}
