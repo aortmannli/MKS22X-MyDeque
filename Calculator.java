d2e3c632336e4ad2d1bc949a27c46494bdabefaa
Angel Ortmann-Li <ali02@stuy.edu>
2019-03-27 13:06:02 -0400

    calc base not working end of period

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,33 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
 public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
-    static String[] op = {"+", "-", "*", "/"}
+    static String[] op = {"+", "-", "*", "/", "%"}
     public static double eval(String s){
       String[] in = String.split(s);
       MyDeque<String> ans = new MyDeque<String>(in.length);
       for(String i : in){
         if(operations.contains(i)){
+          if(i.compareTo("+") == 0){
+            ans.addLast("" + Double.parseDouble(ans.removeLast()) + Double.parseDouble(ans.removeLast());
+          }else if(i.compareTo("-") == 0){
+            ans.addLast("" + Double.parseDouble(ans.removeLast()) + Double.parseDouble(ans.removeLast());
+          }else if(i.compareTo("*") == 0){
+            ans.addLast("" + Double.parseDouble(ans.removeLast()) + Double.parseDouble(ans.removeLast());
+          }else if(i.compareTo("/") == 0){
+            ans.addLast("" + Double.parseDouble(ans.removeLast()) + Double.parseDouble(ans.removeLast());
+          }else if(i.compareTo("%") == 0){
+            ans.addLast("" + Double.parseDouble(ans.removeLast()) + Double.parseDouble(ans.removeLast());
+          }else{
 
+          }
         }
       }
     }
+
+
 }
