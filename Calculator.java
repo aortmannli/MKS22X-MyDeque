9d6cc1a0c918607885cf1c19f40fba19a5af1a83
Angel Ortmann-Li <angel@Valeriyas-MacBook-Air.local>
2019-03-27 20:20:37 -0400

    calculate-or

--- Calculator.java
+++ Calculator.java
@@ -6,27 +6,26 @@ public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
-    static String[] op = {"+", "-", "*", "/", "%"}
+    static String[] op = {"+", "-", "*", "/", "%"};
     public static double eval(String s){
-      String[] in = String.split(s);
+      String[] in = s.split(" ");
       MyDeque<String> ans = new MyDeque<String>(in.length);
       for(String i : in){
-        if(operations.contains(i)){
           if(i.compareTo("+") == 0){
-            ans.addLast("" + Double.parseDouble(ans.removeLast()) + Double.parseDouble(ans.removeLast());
+            ans.addLast("" + Double.parseDouble(ans.removeLast()) + Double.parseDouble(ans.removeLast()));
           }else if(i.compareTo("-") == 0){
-            ans.addLast("" + Double.parseDouble(ans.removeLast()) + Double.parseDouble(ans.removeLast());
+            ans.addLast("" + ((Double.parseDouble(ans.removeLast()) - Double.parseDouble(ans.removeLast())) * -1));
           }else if(i.compareTo("*") == 0){
-            ans.addLast("" + Double.parseDouble(ans.removeLast()) + Double.parseDouble(ans.removeLast());
+            ans.addLast("" + (Double.parseDouble(ans.removeLast()) * Double.parseDouble(ans.removeLast())));
           }else if(i.compareTo("/") == 0){
-            ans.addLast("" + Double.parseDouble(ans.removeLast()) + Double.parseDouble(ans.removeLast());
+            ans.addLast("" + (1/(Double.parseDouble(ans.removeLast()) / Double.parseDouble(ans.removeLast()))));
           }else if(i.compareTo("%") == 0){
-            ans.addLast("" + Double.parseDouble(ans.removeLast()) + Double.parseDouble(ans.removeLast());
+            ans.addLast("" + (Double.parseDouble(ans.removeLast()) % Double.parseDouble(ans.removeLast())));
           }else{
-
+            ans.addLast(i);
           }
-        }
       }
+      return Double.parseDouble(ans.removeLast());
     }
 
 
